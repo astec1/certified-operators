d2130286d1185cda8bbe324495ee4335e3c5c1d4
astec1 <andy.stec@infinite.com>
2025-01-28 11:47:26 -0600

    Add files via upload

--- operators/ndb-operator/cmd/ndb-operator-webhook/main.go
+++ operators/ndb-operator/cmd/ndb-operator-webhook/main.go
@@ -1,1 +1,17 @@
+// Copyright (c) 2021, 2023, Oracle and/or its affiliates.
+//
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
+package main
+
+import (
+	"github.com/mysql/ndb-operator/config"
+	"github.com/mysql/ndb-operator/pkg/webhook"
+	klog "k8s.io/klog/v2"
+)
+
+func main() {
+	klog.Infof("Starting ndb-operator webhook with version %s",
+		config.GetBuildVersion())
+	webhook.Run()
+}
